9f67c43f25b72d667b8831abeb9f0c130f0a6692
wassannc <balaram@wassan.org>
2026-08-05 07:15:05 +0530

    Update estimator.py

--- modules/estimator.py
+++ modules/estimator.py
@@ -1,1 +1,25 @@
+"""
+Estimation Engine
+Version 1
 
+Loads the Excel template and writes mapped values into it.
+"""
+
+from openpyxl import load_workbook
+from config import TEMPLATE_FILE
+
+
+class EstimateGenerator:
+
+    def __init__(self):
+        self.workbook = load_workbook(TEMPLATE_FILE)
+
+    def get_sheet(self, sheet_name):
+        return self.workbook[sheet_name]
+
+    def write_value(self, sheet_name, cell, value):
+        sheet = self.get_sheet(sheet_name)
+        sheet[cell] = value
+
+    def save(self, filename):
+        self.workbook.save(filename)
