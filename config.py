c0a6b96e66d8018cff0f68d32212166c0e16c3f1
wassannc <balaram@wassan.org>
2026-08-05 06:37:59 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,17 +1,17 @@
 # -------- ODK CENTRAL -----------------
 
-ODK_URL = "https://central.wassan.org/"
+ODK_URL = "https://central.wassan.org/projects/"
 
 USERNAME = "balaram@wassan.org"
 
 PASSWORD = "Varahi@2025"
 
-PROJECT_ID = 1
+PROJECT_ID = 6
 
 # Form IDs
-BASIC_FORM_ID = "basic_information"
+BASIC_FORM_ID = "1. Basic Information"
 
-REPAIR_FORM_ID = "rejuvenation_works_repairs"
+REPAIR_FORM_ID = "2. Rejuvenation Works-Repairs"
 
 
 # -------- FILES -----------------------
