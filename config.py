3f1ee06feac7cfa090f7d39d6c8063e81f558504
wassannc <balaram@wassan.org>
2026-08-05 06:32:57 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,32 @@
+# -------- ODK CENTRAL -----------------
 
+ODK_URL = "https://central.wassan.org/"
+
+USERNAME = "balaram@wassan.org"
+
+PASSWORD = "Varahi@2025"
+
+PROJECT_ID = 1
+
+# Form IDs
+BASIC_FORM_ID = "basic_information"
+
+REPAIR_FORM_ID = "rejuvenation_works_repairs"
+
+
+# -------- FILES -----------------------
+
+TEMPLATE_FILE = "templates/estimate_template.xlsx"
+
+MAPPING_FILE = "mapping/field_mapping.xlsx"
+
+OUTPUT_FOLDER = "output"
+
+DATA_FOLDER = "data"
+
+GIS_FOLDER = "data/gis"
+
+
+# -------- FUTURE ----------------------
+
+USE_GIS = False
