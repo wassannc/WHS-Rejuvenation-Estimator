5d1f1797e1821594d58e8b14364a9f2c3bf601fe
wassannc <balaram@wassan.org>
2026-08-06 07:21:36 +0530

    Update processor.py

--- modules/processor.py
+++ modules/processor.py
@@ -1,49 +1,28 @@
 """
-processor.py
-
-Converts ODK data into a Structure Object.
+Repair Data Processor
 """
 
 import pandas as pd
 
 
-class StructureProcessor:
+class RepairProcessor:
 
-    def __init__(self, basic_df, repair_df):
-        self.basic_df = basic_df
-        self.repair_df = repair_df
+    def __init__(self, repairs_df):
+        self.repairs = repairs_df
 
-    def get_structure(self, village):
+    def filter_structure(
+        self,
+        district,
+        block,
+        gp,
+        village
+    ):
 
-        # -------- Basic Information --------
-
-        basic = self.basic_df[
-            self.basic_df["village"] == village
+        df = self.repairs[
+            (self.repairs["basic_details_repairs-district"] == district) &
+            (self.repairs["basic_details_repairs-block"] == block) &
+            (self.repairs["basic_details_repairs-gp"] == gp) &
+            (self.repairs["basic_details_repairs-village"] == village)
         ]
 
-        if basic.empty:
-            raise Exception(f"{village} not found")
-
-        basic = basic.iloc[0].to_dict()
-
-        # -------- Repairs --------
-
-        repairs = self.repair_df[
-            self.repair_df["village"] == village
-        ]
-
-        structure = {
-
-            "basic": basic,
-
-            "repairs": repairs,
-
-            "gis": {},
-
-            "calculations": {},
-
-            "estimate": {}
-
-        }
-
-        return structure
+        return df.reset_index(drop=True)
