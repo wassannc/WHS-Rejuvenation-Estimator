584c9c2921ecab9fccc8766ca11e68f47279668a
wassannc <balaram@wassan.org>
2026-09-02 15:07:50 +0530

    Update mapper.py

--- modules/mapper.py
+++ modules/mapper.py
@@ -55,9 +55,4 @@ class FieldMapper:
         """
         Returns repeat mapping rows from the Repeat Mapping sheet.
         """
-        return self.mapping
-
-    if __name__ == "__main__":
-        mapper = FieldMapper()
-        print("\nREPEAT MAPPING:")
-        print(mapper.get_repeat_mapping().to_string(index=False))
+        return self.repeat_mapping
