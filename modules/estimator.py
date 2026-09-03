84fad600643d1f61f1c3176e3b536c3c31edd2fd
wassannc <balaram@wassan.org>
2026-09-03 17:00:12 +0530

    Update estimator.py

--- modules/estimator.py
+++ modules/estimator.py
@@ -137,6 +137,7 @@ class EstimateGenerator:
             )
 
             output_row += 1
+        return output_row
     def populate_ncg_repeat(self, repeat_records, start_row=2):
         """
         Write NCG repeat records into Repeat Details.
